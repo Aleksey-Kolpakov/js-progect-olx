5db6dcc22dd55abef9ae5d82f175fa64321ba459
Yegor Gangalo <gangaloyegor@gmail.com>
2021-02-02 13:56:59 +0200

    made hero styles & fetch call/ads

---
+++ src/components/hero-section/hero-section.js
@@ -0,0 +1,45 @@
+import './_hero-section-styles.scss';
+import adsCardHandleBar from './hero.hbs';
+import { callAdsAPI } from './fetchAPI';
+
+const heroListRef = document.querySelector('.hero-list');
+callAdsAPI().then(data => data.filter((item, index) => {
+    if (index <= 5) {
+        item.id = index+1;
+        return item;
+    }
+}))
+    .then(data => {
+    const markUp = adsCardHandleBar(data);
+        heroListRef.insertAdjacentHTML('beforeend', markUp);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { callAPI, callFindAPI, callCategoriesAPI, callRussianCategoriesAPI, callSpecificCategoryAPI, callAdsAPI } from './fetchAPI';
+// const tryFetch = () => {
+//     callAPI(1);
+//     callFindAPI('car');
+//     callCategoriesAPI();
+//     callRussianCategoriesAPI();
+//     callSpecificCategoryAPI();
+//     callAdsAPI();
+// };
