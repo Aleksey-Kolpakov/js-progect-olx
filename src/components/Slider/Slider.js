772c2c30c225ba3c29b86de20447767a25e904fb
Yegor Gangalo <gangaloyegor@gmail.com>
2021-02-04 01:56:21 +0200

    separate functions in slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,122 +6,116 @@ export default class Slider {
   }
 
     getRefs(listSelectorCSS, buttons) {
-        const refs = {};
-        refs.sliderList = typeof listSelectorCSS==='object' ? listSelectorCSS :
-            document.querySelector(listSelectorCSS);
-        refs.sliderList.classList.add('slider-wrap');
+      const refs = {};
+      refs.sliderList = typeof listSelectorCSS === 'object'
+        ? listSelectorCSS
+        : document.querySelector(listSelectorCSS);
+      refs.sliderList.classList.add('slider-wrap');
 
-    refs.sliderBlock = document.createElement('div');
-    refs.sliderBlock.classList.add('slider');
-    refs.sliderList.parentNode.insertBefore(refs.sliderBlock, refs.sliderList);
-    refs.sliderBlock.append(refs.sliderList);
+      refs.sliderBlock = document.createElement('div');
+      refs.sliderBlock.classList.add('slider');
+      refs.sliderList.parentNode.insertBefore(refs.sliderBlock, refs.sliderList);
+      refs.sliderBlock.append(refs.sliderList);
 
-        const itemCollection = refs.sliderList.children;
-        itemCollection.forEach(item => item.classList.add('slider-item'));
+      const itemCollection = refs.sliderList.children;
+      itemCollection.forEach(item => item.classList.add('slider-item'));
 
-        /* -------- */
-        const parentWidth = refs.sliderList.offsetWidth;
-        const itemWidth = itemCollection[0]?.offsetWidth;
-        const itemStyles = window.getComputedStyle(itemCollection[0]);
-        const itemMarginLeft = Number.parseInt(itemStyles.marginLeft);
-        const itemMarginRight = Number.parseInt(itemStyles.marginRight);
-        const itemMarginSum = itemMarginRight + itemMarginLeft;
-        const lengthToScroll = parentWidth + itemMarginSum;//довжина, на яку треба прокрутити слайд
-        const itemOnScreen = lengthToScroll / (itemWidth + itemMarginSum);
-        const itemAllAmount = itemCollection.length;
-        const slidesAmount = Math.ceil(itemAllAmount / itemOnScreen);//к-сть прокруток слайду.
+      /* -------- */
+      const parentWidth = refs.sliderList.offsetWidth;
+      const itemWidth = itemCollection[0]?.offsetWidth;
+      const itemStyles = window.getComputedStyle(itemCollection[0]);
+      const itemMarginLeft = Number.parseInt(itemStyles.marginLeft);
+      const itemMarginRight = Number.parseInt(itemStyles.marginRight);
+      const itemMarginSum = itemMarginRight + itemMarginLeft;
+      const lengthToScroll = parentWidth + itemMarginSum;//довжина, на яку треба прокрутити слайд
+      const itemOnScreen = lengthToScroll / (itemWidth + itemMarginSum);
+      const itemAllAmount = itemCollection.length;
+      const slidesAmount = Math.ceil(itemAllAmount / itemOnScreen);//к-сть прокруток слайду.
+      /* -------- */
 
-        console.log('parentWidth',parentWidth);
-        console.log('itemWidth',itemWidth);
-        console.log('itemMarginSum',itemMarginSum);
-        console.log('lengthToScroll',lengthToScroll);
-        console.log('itemOnScreen',itemOnScreen);
-        console.log('slidesAmount',slidesAmount);
-        /* -------- */
-        function createDots(amountOfSlides, parentNode){
-        const dotsArray = [];
-        for (let i = 0; i < amountOfSlides; i += 1){
-            const newDot = document.createElement('div');
-            newDot.classList.add('slider-dot');
-            if (i == 0) {
-                newDot.classList.add('dot-active');
-            };
-            dotsArray.push(newDot);
-        }
-        refs.blockDots = document.createElement('div');
-        refs.blockDots.append(...dotsArray);
-        refs.blockDots.classList.add('slider-dots-block');
-        parentNode.append(refs.blockDots);
-    }
-
-    function createButtons() {
-        refs.prevButton = document.createElement('div');
-        refs.nextButton = document.createElement('div');
-
-        refs.prevButton.insertAdjacentHTML(
-          'beforeend',
-          `<svg class="arrow-icon">
-                      <use href="../images/sprite/sprite.svg#icon-chevron_left" />
-                    </svg>`,
-        );
-        refs.nextButton.insertAdjacentHTML(
-          'beforeend',
-          `<svg class="arrow-icon">
-                      <use href="../images/sprite/sprite.svg#icon-chevron_right" />
-                    </svg>`,
-        );
-
-        refs.buttonsBlock = document.createElement('div');
-        refs.buttonsBlock.classList.add('slider-buttons-block');
-        refs.sliderBlock.parentNode.insertBefore( refs.buttonsBlock, refs.sliderBlock );
-        refs.buttonsBlock.append(refs.sliderBlock);
-
-        refs.prevButton.classList.add('button-prev');
-        refs.nextButton.classList.add('button-next');
-        refs.buttonsBlock.append(...[refs.prevButton, refs.nextButton]);
-    }
-
-        const screenWidth = window.innerWidth;
+      const screenWidth = window.innerWidth;
+      const slideRightCallback = () => (this.slideRight(slidesAmount, lengthToScroll));
+      const slideLeftCallback = () => (this.slideLeft(slidesAmount, lengthToScroll));
         if (buttons && screenWidth>=768) {
-            createButtons();
-            // refs.nextButton.addEventListener('click', this.slideRight.bind(this));
-            // refs.prevButton.addEventListener('click', this.slideLeft.bind(this));
-            refs.nextButton.addEventListener('click', ()=>(this.slideRight(slidesAmount, lengthToScroll)));
-            refs.prevButton.addEventListener('click', ()=>(this.slideLeft(slidesAmount, lengthToScroll)));
+          const { prevButton, nextButton } = this.createButtons(refs.sliderBlock);
+            nextButton.addEventListener('click', slideRightCallback);
+            prevButton.addEventListener('click', slideLeftCallback);
         }
         if (buttons && screenWidth<768 || !buttons) {
-            createDots(slidesAmount, refs.sliderBlock);
-            // refs.sliderBlock.addEventListener('click', this.slideRight.bind(this));
-            refs.sliderBlock.addEventListener('click', ()=>(this.slideRight(slidesAmount, lengthToScroll)));
+          refs.blockDots = this.createDots(slidesAmount);
+          refs.sliderBlock.append(refs.blockDots);
+          refs.sliderBlock.addEventListener('click', slideRightCallback);
         }
 
         return refs;
   }
+/* -------------------------------------------------- */
+  position = 0;
 
-    position = 0;
-
-    slideRight(maxPos, scrollLength) {
-        this.position += 1;
-        // console.log(maxPos);
-        const maxPosition = maxPos - 1;
-        if (this.position > maxPosition) {
-            this.position = 0;
-        };
-        const activeScrollLength = this.position * scrollLength;
-        this.refs.sliderList.style["transform"] = `translateX(calc(-${activeScrollLength}px))`;
-        this.changeActiveDot(this.refs.blockDots, this.position);
+  slideRight(maxPos, scrollLength) {
+    this.position += 1;
+    const maxPosition = maxPos - 1;
+    if (this.position > maxPosition) {
+        this.position = 0;
+    };
+    const activeScrollLength = this.position * scrollLength;
+    this.refs.sliderList.style["transform"] = `translateX(calc(-${activeScrollLength}px))`;
+    this.changeActiveDot(this.refs.blockDots, this.position);
   }
 
-    slideLeft(maxPos, scrollLength) {
-        this.position -= 1;
-        // console.log(maxPos);
-        const maxPosition = maxPos - 1;
-        if (this.position < 0) {
-            this.position = maxPosition;
+  slideLeft(maxPos, scrollLength) {
+    this.position -= 1;
+    const maxPosition = maxPos - 1;
+    if (this.position < 0) {
+        this.position = maxPosition;
+    };
+    const activeScrollLength = this.position * scrollLength;
+    this.refs.sliderList.style["transform"] = `translateX(calc(-${activeScrollLength}px))`;
+    this.changeActiveDot(this.refs.blockDots, this.position);
+  }
+
+  createButtons(sliderBlock) {
+    const prevButton = document.createElement('div');
+    const nextButton = document.createElement('div');
+
+    prevButton.insertAdjacentHTML(
+      'beforeend',
+      `<svg class="arrow-icon">
+                  <use href="../images/sprite/sprite.svg#icon-chevron_left" />
+                </svg>`,
+    );
+    nextButton.insertAdjacentHTML(
+      'beforeend',
+      `<svg class="arrow-icon">
+                  <use href="../images/sprite/sprite.svg#icon-chevron_right" />
+                </svg>`,
+    );
+
+    const buttonsBlock = document.createElement('div');
+    buttonsBlock.classList.add('slider-buttons-block');
+    sliderBlock.parentNode.insertBefore( buttonsBlock, sliderBlock );
+    buttonsBlock.append(sliderBlock);
+
+    prevButton.classList.add('button-prev');
+    nextButton.classList.add('button-next');
+    buttonsBlock.append(...[prevButton, nextButton]);
+    return {prevButton, nextButton};
+  }
+
+  createDots(amountOfSlides){
+    const dotsArray = [];
+    for (let i = 0; i < amountOfSlides; i += 1){
+        const newDot = document.createElement('li');
+        newDot.classList.add('slider-dot');
+        if (i == 0) {
+            newDot.classList.add('dot-active');
         };
-        const activeScrollLength = this.position * scrollLength;
-        this.refs.sliderList.style["transform"] = `translateX(calc(-${activeScrollLength}px))`;
-        this.changeActiveDot(this.refs.blockDots, this.position);
+        dotsArray.push(newDot);
+    }
+    const blockDots = document.createElement('ul');
+    blockDots.classList.add('slider-dots-block');
+    blockDots.append(...dotsArray);
+    return blockDots;
   }
 
   changeActiveDot(blockDots, currentPosition) {
@@ -133,3 +127,54 @@ export default class Slider {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+   // function createDots(amountOfSlides, parentNode){
+        // const dotsArray = [];
+        // for (let i = 0; i < amountOfSlides; i += 1){
+        //     const newDot = document.createElement('li');
+        //     newDot.classList.add('slider-dot');
+        //     if (i == 0) {
+        //         newDot.classList.add('dot-active');
+        //     };
+        //     dotsArray.push(newDot);
+        // }
+        // refs.blockDots = document.createElement('ul');
+        // refs.blockDots.classList.add('slider-dots-block');
+        // refs.blockDots.append(...dotsArray);
+        // parentNode.append(refs.blockDots);
+        // }
+
+    // function createButtons() {
+    //     refs.prevButton = document.createElement('div');
+    //     refs.nextButton = document.createElement('div');
+
+    //     refs.prevButton.insertAdjacentHTML(
+    //       'beforeend',
+    //       `<svg class="arrow-icon">
+    //                   <use href="../images/sprite/sprite.svg#icon-chevron_left" />
+    //                 </svg>`,
+    //     );
+    //     refs.nextButton.insertAdjacentHTML(
+    //       'beforeend',
+    //       `<svg class="arrow-icon">
+    //                   <use href="../images/sprite/sprite.svg#icon-chevron_right" />
+    //                 </svg>`,
+    //     );
+
+    //     refs.buttonsBlock = document.createElement('div');
+    //     refs.buttonsBlock.classList.add('slider-buttons-block');
+    //     refs.sliderBlock.parentNode.insertBefore( refs.buttonsBlock, refs.sliderBlock );
+    //     refs.buttonsBlock.append(refs.sliderBlock);
+
+    //     refs.prevButton.classList.add('button-prev');
+    //     refs.nextButton.classList.add('button-next');
+    //     refs.buttonsBlock.append(...[refs.prevButton, refs.nextButton]);
+    // }
