1fa730b4faef505dc7e0c64561185d5da6b93885
Yegor Gangalo <gangaloyegor@gmail.com>
2021-02-04 03:41:25 +0200

    add slide on dots & delete from main listBlock

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -20,6 +20,7 @@ export default class Slider {
       const itemCollection = refs.sliderList.children;
       itemCollection.forEach(item => item.classList.add('slider-item'));
 
+      // const resizeWindow = () => {
       /* -------- */
       const parentWidth = refs.sliderList.offsetWidth;
       const itemWidth = itemCollection[0]?.offsetWidth;
@@ -33,19 +34,22 @@ export default class Slider {
       const slidesAmount = Math.ceil(itemAllAmount / itemOnScreen);//к-сть прокруток слайду.
       /* -------- */
 
-      const screenWidth = window.innerWidth;
-      const slideRightCallback = () => (this.slideRight(slidesAmount, lengthToScroll));
-      const slideLeftCallback = () => (this.slideLeft(slidesAmount, lengthToScroll));
-        if (buttons && screenWidth>=768) {
+        const screenWidth = window.innerWidth;
+        const slideRightCallback = () => (this.slideRight(slidesAmount, lengthToScroll));
+        const slideLeftCallback = () => (this.slideLeft(slidesAmount, lengthToScroll));
+        if (buttons && screenWidth >= 768) {
           const { prevButton, nextButton } = this.createButtons(refs.sliderBlock);
-            nextButton.addEventListener('click', slideRightCallback);
-            prevButton.addEventListener('click', slideLeftCallback);
+          nextButton.addEventListener('click', slideRightCallback);
+          prevButton.addEventListener('click', slideLeftCallback);
         }
-        if (buttons && screenWidth<768 || !buttons) {
-          refs.blockDots = this.createDots(slidesAmount);
+        if (buttons && screenWidth < 768 || !buttons) {
+          refs.blockDots = this.createDots(slidesAmount,lengthToScroll,refs.sliderList);
           refs.sliderBlock.append(refs.blockDots);
-          refs.sliderBlock.addEventListener('click', slideRightCallback);
+          // refs.sliderBlock.addEventListener('click', slideRightCallback);
         }
+      // }
+      // resizeWindow();
+      // window.addEventListener('resize', resizeWindow);
 
         return refs;
   }
@@ -102,26 +106,39 @@ export default class Slider {
     return {prevButton, nextButton};
   }
 
-  createDots(amountOfSlides){
+  createDots(amountOfSlides, scrollLength, sliderBlock){
     const dotsArray = [];
     for (let i = 0; i < amountOfSlides; i += 1){
-        const newDot = document.createElement('li');
-        newDot.classList.add('slider-dot');
-        if (i == 0) {
-            newDot.classList.add('dot-active');
-        };
-        dotsArray.push(newDot);
+      const newDot = document.createElement('li');
+      newDot.setAttribute('data-id', i);
+      newDot.classList.add('slider-dot');
+      if (i == 0) {
+          newDot.classList.add('dot-active');
+      };
+      dotsArray.push(newDot);
     }
     const blockDots = document.createElement('ul');
+    blockDots.addEventListener('click', this.toTargetSlide(scrollLength,sliderBlock));
     blockDots.classList.add('slider-dots-block');
     blockDots.append(...dotsArray);
     return blockDots;
+  }
+
+  toTargetSlide=(scrollLength, sliderBlock)=>(event)=> {
+    if (event.target === event.currentTarget) {
+      return;
+    }
+      this.position = event.target.dataset.id;
+      const activeScrollLength = this.position * scrollLength;
+    sliderBlock.style["transform"] = `translateX(calc(-${activeScrollLength}px))`;
+    this.changeActiveDot(this.refs.blockDots, this.position);
   }
 
   changeActiveDot(blockDots, currentPosition) {
     if (blockDots) {
       const allDots = blockDots.children;
       allDots.forEach(dot => dot.classList.remove('dot-active'));
+      console.log(currentPosition);
       const activeDot = allDots[currentPosition];
       activeDot.classList.add('dot-active');
     }
