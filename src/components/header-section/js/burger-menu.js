dfbbe7056e6be8e5f3f155062f4c238665f2beef
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-02-04 12:29:35 +0200

    сделал мелкие правки

--- src/components/header-section/js/burger-menu.js
+++ src/components/header-section/js/burger-menu.js
@@ -5,14 +5,14 @@ refs.mobileCloseButton.addEventListener('click', closeMobileMenu);
 
 function openMobileMenu() {
   refs.mobileMenu.classList.add('is-open');
-  refs.authenticationBlock.classList.add('is-open');
+  refs.authorizationBlock.classList.add('is-open');
   refs.myCabinetBlock.classList.add('is-open');
   refs.filterClearBlock.classList.add('is-open');
 }
 
 function closeMobileMenu() {
   refs.mobileMenu.classList.remove('is-open');
-  refs.authenticationBlock.classList.remove('is-open');
+  refs.authorizationBlock.classList.remove('is-open');
   refs.myCabinetBlock.classList.remove('is-open');
   refs.filterClearBlock.classList.remove('is-open');
   refs.filtersContainer.classList.remove('is-open');
