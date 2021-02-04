56975dcd8a2baaf8cc517e14a2e9c0551ce7be22
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-02-04 12:27:43 +0200

    раскомментировал свою секцию

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './main.scss';
 import './components/form-change-item/form-change-item.js';
 import './components/form-create-item/form-create-item.js';
 import './components/form-registration/form-registration.js';
-// import './components/header-section/header.js';
+import './components/header-section/header.js';
 import './components/hero-section/hero-section.js';
 import './components/item-card/item-card.js';
 import './pages/main-page/main-section.js';
