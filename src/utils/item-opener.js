46b3de0bc69585887ee11cf3c079b39842bb7d15
Aleksey-Kolpakov <gmsaleksei@gmail.com>
2021-02-05 19:42:57 +0200

    open images func

---
+++ src/utils/item-opener.js
@@ -0,0 +1,17 @@
+import { state } from './backend-services.js'
+const itemOpener = function () {
+    const containersArr = document.querySelectorAll('.js-item-container');
+    containersArr.map(ul => {
+        ul.addEventListener('click', openItemModal);
+    })
+}
+
+function openItemModal(event) {
+    event.preventDefault();
+    const itemRef = event.target.closest("[data-id]");
+    const clickedItemId = item.dataset.id;
+    const itemData = state.find(item => {
+        item._id === clickedItemId
+    });
+    /// здесь нужно вызы
+}
