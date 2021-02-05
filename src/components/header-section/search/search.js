226e0d87fd7427c3ce371dfce55fae9d408dbd04
yankyrychenko <yan7darknet@gmail.com>
2021-02-05 19:31:33 +0200

    done

--- src/components/header-section/search/search.js
+++ src/components/header-section/search/search.js
@@ -4,7 +4,7 @@ import './search.scss';
 import '../../../main.scss';
 
 const mainRef = document.querySelector('.js-main');
-const wrapRef = document.querySelector('.js-search-container');
+const wrapRef = document.querySelector('.search-is-hidden');
 const searchRef = document.querySelector('.js-search');
 const input = document.querySelector('.js-input-search');
 
@@ -31,9 +31,6 @@ function onSearchMarkup(data) {
   }
 
   wrapRef.classList.remove('search-is-hidden');
-  wrapRef.classList.add('search-is-shown');
-  wrapRef.innerHTML = '';
-  mainRef.innerHTML = '';
-  wrapRef.insertAdjacentHTML('beforeend', markup);
+  mainRef.innerHTML = markup;
   input.value = '';
 }
