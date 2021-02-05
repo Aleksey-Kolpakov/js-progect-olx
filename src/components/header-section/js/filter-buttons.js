1ef3e9ef466fc398fbd4d20f1241457e61d37280
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-02-05 15:43:18 +0200

    с Эдиком создали отрисовку на странице

--- src/components/header-section/js/filter-buttons.js
+++ src/components/header-section/js/filter-buttons.js
@@ -4,6 +4,8 @@ import {
   EnglishCategoriesPromise,
 } from '../../../utils/initial-load';
 import { getItemsInCategory } from '../../../utils/backend-services';
+import { closeMobileMenu } from './service';
+import template from '../../../pages/main-page/templates/categories-markup.hbs';
 
 refs.filterList.addEventListener('click', onFilterButtonClick);
 
@@ -15,7 +17,25 @@ function onFilterButtonClick(event) {
       EnglishCategoriesPromise.then(array => {
         console.log(array[index]);
 
-        getItemsInCategory(array[index]).then(array => console.log(array));
+        getItemsInCategory(array[index]).then(array => {
+          const mappedArray = array.map(item => ({
+            ...item,
+            imageUrls: item.imageUrls[0],
+          }));
+
+          const markup = template(mappedArray);
+
+          refs.mainSection.innerHTML = '';
+          refs.mainSection.insertAdjacentHTML('beforeend', markup);
+
+          if (window.innerWidth >= 768 && window.innerWidth < 1280) {
+            refs.filtersContainer.classList.remove('tablet-is-open');
+          }
+
+          if (window.innerWidth < 768) {
+            closeMobileMenu();
+          }
+        });
       });
     });
   }
