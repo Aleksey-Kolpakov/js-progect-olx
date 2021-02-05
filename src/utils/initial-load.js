c69d8d56bdfb71b42a26367635c4475e89ec87ae
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-02-05 14:37:56 +0200

    с Колпаковым делали правки

--- src/utils/initial-load.js
+++ src/utils/initial-load.js
@@ -1,18 +1,31 @@
-import { getRussianCategories, getEnglishCategories, getUsersInfoByToken } from './backend-services';
+import {
+  getRussianCategories,
+  getEnglishCategories,
+  getUsersInfoByToken,
+} from './backend-services';
 import localStoradge from './local-storadge.js';
 export const RussianCategoriesPromise = getRussianCategories();
 export const EnglishCategoriesPromise = getEnglishCategories();
 
-export function isUserAutorized() {
-    const accesToken = localStoradge.load('accessTokenOlx');
-    if (accesToken) {
-        return getUsersInfoByToken();
-    }
-    return Promise.reject(new Error('Ashibka'));
-};
+import {
+  showAuthorizationBlock,
+  showMyCabinetBlock,
+} from '../components/header-section/js/authorization-my-cabinet-blocks';
 
-isUserAutorized().then(data => {
-    console.log('Рендер Юзера ')
-}).catch(data => {
+export function isUserAutorized() {
+  const accesToken = localStoradge.load('accessTokenOlx');
+  if (accesToken) {
+    return getUsersInfoByToken();
+  }
+  return Promise.reject(new Error('Ashibka'));
+}
+
+isUserAutorized()
+  .then(data => {
+    console.log('Рендер Юзера ');
+    showMyCabinetBlock();
+  })
+  .catch(data => {
     console.log('Рендер логин/Рега');
-});
+    showAuthorizationBlock();
+  });
