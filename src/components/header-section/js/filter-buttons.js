1ed821de9250bb274ac08734e6d12e48f94dbd2a
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-02-05 14:53:57 +0200

    Update filter-buttons.js

--- src/components/header-section/js/filter-buttons.js
+++ src/components/header-section/js/filter-buttons.js
@@ -1,18 +1,18 @@
 import refs from './refs';
 import {
-  getRussianCategories,
-  getEnglishCategories,
-  getItemsInCategory,
-} from '../../../utils/backend-services';
+  RussianCategoriesPromise,
+  EnglishCategoriesPromise,
+} from '../../../utils/initial-load';
+import { getItemsInCategory } from '../../../utils/backend-services';
 
 refs.filterList.addEventListener('click', onFilterButtonClick);
 
 function onFilterButtonClick(event) {
   if (event.target.nodeName === 'BUTTON') {
-    getRussianCategories().then(array => {
+    RussianCategoriesPromise.then(array => {
       const index = array.indexOf(event.target.textContent.trim()); // trim() - убирает отступы справа и слева (текстовые узлы)
 
-      getEnglishCategories().then(array => {
+      EnglishCategoriesPromise.then(array => {
         console.log(array[index]);
 
         getItemsInCategory(array[index]).then(array => console.log(array));
