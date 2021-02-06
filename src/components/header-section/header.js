99c580e0564d629d9f137d67952dceeae97accca
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-02-06 13:36:32 +0200

    Update header.js

--- src/components/header-section/header.js
+++ src/components/header-section/header.js
@@ -3,7 +3,6 @@ import './js/burger-menu';
 import './js/input';
 import './js/mobile-search';
 import './js/authorization';
-// import './js/my-cabinet';
 import './js/create-ad';
 import './js/filters-container';
 import './js/filter-buttons';
