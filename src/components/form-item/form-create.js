a66bf7874c35f3f24caf536cdbc7808d7d0c9295
Anur13 <akrupskyi13@gmail.com>
2021-02-09 12:42:52 +0200

    Done

--- src/components/form-item/form-create.js
+++ src/components/form-item/form-create.js
@@ -16,47 +16,67 @@ export function addRusCategory() {
     const template = templateCategory(data);
     categoryRef.insertAdjacentHTML('beforeend', template);
     return;
-  })
+  });
 }
 
 export function sendItemOnServer() {
   const form = document.querySelector('.form');
 
   //------------------------------------------- Список доступных категорий (должно совпадать с бэкэндом)
-  
+
   const listOfCategory = {
-  "Недвижимость": "property",
-  "Транспорт": "transport",
-  "Работа": "work",
-  "Электроника": "electronics",
-  "Бизнес и услуги": "business and services",
-  "Отдых и спорт": "recreation and sport",
-  "Отдам бесплатно": "free",
-  "Обмен": "trade",
-}
+    Недвижимость: 'property',
+    Транспорт: 'transport',
+    Работа: 'work',
+    Электроника: 'electronics',
+    'Бизнес и услуги': 'business and services',
+    'Отдых и спорт': 'recreation and sport',
+    'Отдам бесплатно': 'free',
+    Обмен: 'trade',
+  };
   //-------------------------------------Переводчик категорий для отправки на бэкэнд
-  
+
   function translator(rus, list) {
     if (listOfCategory[rus] === undefined) {
-      console.warn('Отсутствует перевод. Отредактируйте список категорий - listOfCategory (form-create)')
+      console.warn(
+        'Отсутствует перевод. Отредактируйте список категорий - listOfCategory (form-create)',
+      );
     }
     return listOfCategory[rus];
   }
-  
+
   //------------------------------------------- ф-я сбора всех полей
-  
+
   function formDataCollect(event) {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const submitData = {};
+    const downloadInput = document.querySelector('.download__input');
 
-    formData.forEach((value, key) => { submitData[key] = value;});
-    submitData.category = translator(submitData.category, listOfCategory);
-    console.log(submitData)
+    const formData = new FormData();
 
-    createItemFetch(submitData);
-    return submitData;
+    const foData = new FormData(event.target);
+    foData.forEach((value, key) => {
+      if (key !== 'file') {
+        formData.set(key, value);
+        if (listOfCategory.hasOwnProperty(value)) {
+          // console.log('success');
+          formData.set('category', listOfCategory[value]);
+        }
+      }
+    });
+    // console.log(listOfCategory.Недвижимость);
+    downloadInput.files.forEach(file => {
+      formData.append('file', file);
+    });
+
+    var object = {};
+    formData.forEach(function (value, key) {
+      object[key] = value;
+    });
+    var json = JSON.stringify(object);
+    console.log(json);
+
+    createItemFetch(formData);
   }
-  
-  form.addEventListener("submit", formDataCollect);
+
+  form.addEventListener('submit', formDataCollect);
 }
