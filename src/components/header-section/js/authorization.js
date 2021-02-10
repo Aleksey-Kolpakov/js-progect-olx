4308adf89b229576640b4ca6e022a744bce39fb0
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-02-10 19:24:12 +0200

    внес очистку фильтра при кликах на другие кнопки

--- src/components/header-section/js/authorization.js
+++ src/components/header-section/js/authorization.js
@@ -1,8 +1,11 @@
 import refs from './refs';
-import { closeMobileMenu } from './service';
+import { closeMobileMenu, clearFilterButtons } from './service';
 import { openForm } from '../../form-registration/form-registration';
 
 refs.registrationButton.addEventListener('click', openForm);
 refs.loginButton.addEventListener('click', openForm);
 
-refs.myCabinetButton.addEventListener('click', closeMobileMenu);
+refs.myCabinetButton.addEventListener('click', () => {
+  closeMobileMenu();
+  clearFilterButtons();
+});
